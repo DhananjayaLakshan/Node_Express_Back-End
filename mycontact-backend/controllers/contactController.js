7e60c8dd2140f1a5f34c7c006b99525cb7929375
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-12-31 20:09:58 +0530

    connected to database

--- mycontact-backend/controllers/contactController.js
+++ mycontact-backend/controllers/contactController.js
@@ -1,22 +1,24 @@
+const asyncHandler = require('express-async-handler')//this package import to handle error
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler ( async (req, res) => {
     res.status(200).jason({ message: "Get all contacts" })
-}
+})
 
 //@desc Get a contact
 //@route GET /api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler (async (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` })
-}
+})
 
 //@desc Create a contact
 //@route POST /api/contacts
 //@access public
 //201 = resource created
-const createContact = (req, res) => {
+const createContact = asyncHandler (async (req, res) => {
     console.log("The request body is : ", req.body);
     const {name, email, phone} = req.body
 
@@ -25,21 +27,21 @@ const createContact = (req, res) => {
         throw new Error("All the fields are mandatory !")
     }
     res.status(201).json({ message: 'Create contacts' })
-}
+})
 
 //@desc Update a contact
 //@route PUT /api/contacts
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler (async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` })
-}
+})
 
 //@desc delete a contact
 //@route DELETE /api/contacts
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler (async (req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` })
-}
+})
 
 module.exports = {
     getContacts,
--- mycontact-backend/server.js
+++ mycontact-backend/server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv').config()
 
 const contactRoutes = require('./routes/contactRoutes')
 const errorHandler = require('./middleware/errorHandler')
+const connectDB = require('./config/dbConnection')
 
+connectDB()
 const app = express()
 
 const port = process.env.PORT
